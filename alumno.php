97fe07a2930afb91b3cb0caa7214bc251bad4c78
pedroasg <158416336+pedroasg1@users.noreply.github.com>
2024-11-06 13:49:26 -0600

    Update alumno.php

--- alumno.php
+++ alumno.php
@@ -1,30 +1,42 @@
 <?php
+// Conexión a la base de datos
 $con = mysqli_connect("localhost", "root", "", "colegio");
 if(!$con) {
-    die('No se establecio la conexion en el servidor. '. mysqli_error());
-}
-$sql = "INSERT INTO alumno VALUES('$_POST[id]', '$_POST[nombre]', '$_POST[edad]', '$_POST[direccion]', '$_POST[fecha_registro]')";
-if(!mysqli_query($con, $sql, MYSQLI_USE_RESULT)){
-    die('Error: '.mysqli_error($con));
+    die('No se estableció la conexión en el servidor. '. mysqli_connect_error());
 }
 
-if (isset($_POST['id'])) {
-    $id = $_POST['id'];
-    $sql = "DELETE FROM alumno WHERE id='$id'";
-
-    if (!mysqli_query($con, $sql)) {
-        die('Error: ' . mysqli_error($con));
+// Insertar registro
+if (isset($_POST['id']) && isset($_POST['nombre']) && isset($_POST['edad']) && isset($_POST['direccion']) && isset($_POST['fecha_registro'])) {
+    // Sentencia preparada para evitar inyección SQL
+    $stmt = $con->prepare("INSERT INTO alumno (id, nombre, edad, direccion, fecha_registro) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssiss", $_POST['id'], $_POST['nombre'], $_POST['edad'], $_POST['direccion'], $_POST['fecha_registro']);
+    
+    if ($stmt->execute()) {
+        echo "<center>1 registro agregado<br>";
+        echo "<a href='./index.html'>Insertar otro registro</a></center>";
+    } else {
+        echo "Error al insertar el registro: " . $stmt->error;
     }
-
-    echo "<center>";
-    echo "Registro con ID $id eliminado<br>";
-    echo "<a href='index.html'>Eliminar otro registro</a>";
-} else {
-    echo "ID no proporcionado.";
+    $stmt->close();
 }
 
-echo "<center>";
-echo "1 registro agregado<br>";
-echo "<a href='index.html'>Insertar registro</a>";
+// Eliminar registro
+if (isset($_POST['id_eliminar'])) {
+    $id = $_POST['id_eliminar'];
+    
+    // Sentencia preparada para eliminar el registro por ID
+    $stmt = $con->prepare("DELETE FROM alumno WHERE id = ?");
+    $stmt->bind_param("s", $id);
+
+    if ($stmt->execute()) {
+        echo "<center>Registro con ID $id eliminado<br>";
+        echo "<a href='./index.html'>Eliminar otro registro</a></center>";
+    } else {
+        echo "Error al eliminar el registro: " . $stmt->error;
+    }
+    $stmt->close();
+}
+
+// Cerrar la conexión
 mysqli_close($con);
 ?>
